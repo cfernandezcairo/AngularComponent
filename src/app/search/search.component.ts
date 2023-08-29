9cb35dd4e756a46932a024b910aacf154d651d1c
cfernandezcairo <cfernandezcairo@gmail.com>
2023-08-28 21:48:52 -0400

    disable/enable span element

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
-  searchValue: string = "iphone"
+  searchValue: string = ""
   changeSearchValue(eventData: Event) {
     
     //EventBinding
