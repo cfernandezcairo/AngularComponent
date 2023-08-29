5c3060e06172b1162e7a5448af82e4aa1d4a6a61
cfernandezcairo <cfernandezcairo@gmail.com>
2023-08-28 21:05:41 -0400

    change backgroundColor

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class ProductsComponent {
   products = [
     {id:1, name: 'Product1', price: '100', color: ' black', available: 'Available', image: '/assets/products/product-image-1.jpg'},
-    {id:1, name: 'Product2', price: '200', color: ' bluw', available: 'Not Available', image: '/assets/products/product-image-2.jpg'},
+    {id:1, name: 'Product2', price: '200', color: ' blue', available: 'Not Available', image: '/assets/products/product-image-2.jpg'},
     {id:1, name: 'Product3', price: '300', color: ' red', available: 'Available', image: '/assets/products/product-image-3.jpg'}
   ];
 }
